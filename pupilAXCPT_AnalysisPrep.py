9bbb649b3b9cef1a8dce5282514d549fc8b9151e
jelman <jelman523@gmail.com>
2017-01-04 08:59:27 -0800

    Use practice effect adjusted data and remove unneeded variables

--- pupilAXCPT_AnalysisPrep.py
+++ pupilAXCPT_AnalysisPrep.py
@@ -4,7 +4,7 @@ Created on Fri Jul 24 11:53:55 2015
 
 @author: jaelman
 
-Create dataset for analysis. Combines data from AX-CPT, cognitive tasks, 
+Create dataset for analysis. Combines data from AX-CPT, cognitive tasks,
 and pupillometry.
 """
 
@@ -14,64 +14,62 @@ from sas7bdat import SAS7BDAT
 import os
 
 ###############################
-datadir = 'K:/Projects/Pupil_AX-CPT/data'
-outdir = 'K:/Projects/Pupil_AX-CPT/data'
-pupil_fname = 'K:/data/Pupillometry/pupilDS_long.csv'
-cogv2_fname = 'K:/data/VETSA2_April2015/vetsa2merged_1dec2015_edits.sas7bdat'
-cogv1_fname = 'K:/data/VETSA1_Aug2014/vetsa1merged_21aug2014.sas7bdat'
-cogVars_fname = 'K:/Projects/Pupil_AX-CPT/data/AX-CPT_CogVariables.csv'
-demo_fname = 'K:/data/VETSA_demo_vars.csv'
-axcpt_fname = 'K:/data/AX-CPT/AX-CPT_V2_processed.csv'
-mci_fname = 'K:/data/VETSA2_MCI.csv'
+datadir = '~/netshare/K/Projects/Pupil_AX-CPT/data'
+outdir = '~/netshare/K/Projects/Pupil_AX-CPT/data'
+pupil_fname = '~/netshare/K/data/Pupillometry/VETSA2/pupilDS_long.csv'
+cog_fname = '/home/jelman/netshare/M/PSYCH/KREMEN/Practice Effect Cognition/data/V1V2_CogData_NASAdj.csv'
+demo_fname = '~/netshare/K/data/VETSA_Demographics/VETSA_demo_vars2.csv'
+axcpt_fname = '~/netshare/K/data/AX-CPT/AX-CPT_V2.csv'
+mci_fname = '~/netshare/K/data/VETSA_Demographics/VETSA2_MCI.csv'
 cog_outname = 'AX-CPT_cogData.csv'
 final_outname = 'pupil_AX-CPT.csv'
 ###############################
-
 
 ### Get cognitive and demographic data ###
 
 # Load demographic data
 demodf = pd.read_csv(demo_fname)
-
-# Load vetsa2merged dataset to get head injury data
-with SAS7BDAT(cogv2_fname) as f:
-    cogdf = f.to_data_frame()
-    
-cogdf = cogdf[['vetsaid','DSFMAX_v2','afqtpcttran_v2','HADSHINJ_v2',
-               'NUMHINJ_v2']]
-
-# Set missing missing values in head injury variables
-cogdf.ix[cogdf['HADSHINJ_v2']==9,'HADSHINJ_v2'] = None
-cogdf.ix[cogdf['NUMHINJ_v2']==99,'NUMHINJ_v2'] = None
+demodf.columns
+# Load practice effects data to get AX-CPT and max digit span data
+cogdf = pd.read_csv(cog_fname)
+cols = ['VETSAID','NAS201TRAN','DSFMAX_V2_nasp','AXHITRATE_V2_nasp',
+        'AXFARATE_V2_nasp','AXMISSRATE_V2_nasp','BXHITRATE_V2_nasp',
+        'BXFARATE_V2_nasp','BXMISSRATE_V2_nasp','CPTDPRIME_V2_nasp']
+cogdf.columns
+cogdf = cogdf.loc[:,cols]
+cogdf.rename(columns={'VETSAID':'vetsaid'}, inplace=True)
 
 # Merge demographic and cognitive data
-cogdf = demodf.merge(cogdf, how='right', on='vetsaid')
-      
+cogdf = demodf.merge(cogdf, how='left', on='vetsaid')
+
 # Save out cognitive data
 cog_outfile = os.path.join(datadir, cog_outname)
 cogdf.to_csv(cog_outfile, index=False)
 
 ## Load pupil data
 pupildf = pd.read_csv(pupil_fname)
+pupildf.columns
 pupildf = pupildf.drop(['case','twin','zyg14'], axis=1)
+
 ## Load AX-CPT data
 axcptdf = pd.read_csv(axcpt_fname)
+
+#Filter out subjects who were given a Z score of 2 or were not completed
+axcptdf = axcptdf.loc[(axcptdf['ZAXCPT_v2']!=2) &
+                      (axcptdf['CPTCOMPLETE_v2']==0)]
+axcptdf = pd.DataFrame(axcptdf['vetsaid'])
 
 # Load MCI data
 MCIdf = pd.read_csv(mci_fname)
 
-#Filter out subjects who were given a Z score of 2 or were not completed
-axcptdf = axcptdf.loc[(axcptdf['ZAXCPT_v2']!=2) & 
-                      (axcptdf['CPTCOMPLETE_v2']==0)]
-
 ## Merge datasets
-axcpt_cog = pd.merge(axcptdf, cogdf, left_on='vetsaid', 
-                   right_on='vetsaid', how='left')        
-pupil_axcpt = pd.merge(pupildf, axcpt_cog, left_on='vetsaid', 
-                   right_on='vetsaid', how='inner')                  
-pupil_axcpt = pd.merge(pupil_axcpt, MCIdf[['vetsaid','rMCI_cons_v2']], 
-                         left_on='vetsaid',right_on='vetsaid', how='left') 
-                                                
+axcpt_cog = pd.merge(axcptdf, cogdf, left_on='vetsaid',
+                   right_on='vetsaid', how='left')
+pupil_axcpt = pd.merge(pupildf, axcpt_cog, left_on='vetsaid',
+                   right_on='vetsaid', how='inner')
+pupil_axcpt = pd.merge(pupil_axcpt, MCIdf[['vetsaid','rMCI_cons_v2pe']],
+                         left_on='vetsaid',right_on='vetsaid', how='left')
+
 # Save out files
 outfile = os.path.join(outdir,final_outname)
-pupil_axcpt.to_csv(outfile, index=False)                                                
+pupil_axcpt.to_csv(outfile, index=False)
